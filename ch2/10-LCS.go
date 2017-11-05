2d7823f5bddb72d4696ec54669202edcb66eb877
jagijagijag1 <kiyo070707+jagithub@gmail.com>
2017-11-05 14:15:39 +0900

    MOD: change indexing

--- ch2/10-LCS.go
+++ ch2/10-LCS.go
@@ -5,15 +5,16 @@ import "fmt"
 var lcs_dp [][]int
 
 func solveLCS(n, m int, s, t string) int {
+	s, t = "0"+s, "0"+t
 	initLCSDP()
-	for i := 0; i < n; i++ {
-		for j := 0; j < m; j++ {
+	for i := 1; i < n+1; i++ {
+		for j := 1; j < m+1; j++ {
 			//fmt.Println("i =", i, ", j =", j)
 			if s[i] == t[j] {
-				lcs_dp[i+1][j+1] = lcs_dp[i][j] + 1
+				lcs_dp[i][j] = lcs_dp[i-1][j-1] + 1
 				//fmt.Println("  (", i+1, ", ", j+1, ") is ", lcs_dp[i][j], " + 1")
 			} else {
-				lcs_dp[i+1][j+1] = max(lcs_dp[i][j+1], lcs_dp[i+1][j])
+				lcs_dp[i][j] = max(lcs_dp[i-1][j], lcs_dp[i][j-1])
 				//fmt.Println("  (", i+1, ", ", j+1, ") is ", lcs_dp[i][j+1], " or ", lcs_dp[i+1][j])
 			}
 			//printLCSDP(n+1, m+1, s, t)
@@ -36,10 +37,9 @@ func initLCSDP() {
 }
 
 func printLCSDP(n, m int, s, t string) {
-	ss, st := "0"+s, "0"+t
-	fmt.Println(" ", st)
+	fmt.Println(" ", t)
 	for i := 0; i < n; i++ {
-		fmt.Print(string(ss[i]), " ")
+		fmt.Print(string(s[i]), " ")
 		for j := 0; j < m; j++ {
 			fmt.Print(lcs_dp[i][j], "")
 		}
