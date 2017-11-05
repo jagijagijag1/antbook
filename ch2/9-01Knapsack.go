2053f7ee88f915f2b067a66392f5cb0f2b619ebe
jagijagijag1 <kiyo070707+jagithub@gmail.com>
2017-11-05 12:45:45 +0900

    MOD: use for loop instead of recursive func

--- ch2/9-01Knapsack.go
+++ ch2/9-01Knapsack.go
@@ -5,34 +5,22 @@ import "fmt"
 const MAX_N, MAX_W = 100, 100
 
 var dp [][]int
-var n, W int
-var w, v []int
 
-func rec(i int, j int) int {
-	if dp[i][j] >= 0 {
-		return dp[i][j]
-	}
-
-	var res int
-	if i == n {
-		res = 0
-	} else if j < w[i] {
-		res = rec(i+1, j)
-	} else {
-		res = max(rec(i+1, j), rec(i+1, j-w[i])+v[i])
-	}
-
-	dp[i][j] = res
-	return res
-}
-
-func solve01Knapsack(n_in int, w_in []int, v_in []int, W_in int) int {
-	n, w, v, W = n_in, w_in, v_in, W_in
+func solve01Knapsack(n int, w []int, v []int, W int) int {
 	initDP()
 
-	ans := rec(0, W)
-	fmt.Println(ans)
-	return ans
+	for i := n - 1; i >= 0; i-- {
+		for j := 0; j <= W; j++ {
+			if j < w[i] {
+				dp[i][j] = dp[i+1][j]
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i+1][j-w[i]]+v[i])
+			}
+		}
+	}
+
+	fmt.Println(dp[0][W])
+	return dp[0][W]
 }
 
 func initDP() {
@@ -40,7 +28,7 @@ func initDP() {
 	for i := 0; i < MAX_N; i++ {
 		dp[i] = make([]int, MAX_W+1)
 		for j := 0; j < MAX_W; j++ {
-			dp[i][j] = -1
+			dp[i][j] = 0
 		}
 	}
 }
